e3d7613060fdda971a780a3b1b6c2d01352dd772
Sebastian <sebastian.vallejos2001@alumnos.ubiobio.cl>
2025-03-12 11:20:56 -0300

    Mostrar imagen o gif aleatorio al iniciar la pagina

--- flutterapp/lib/main.dart
+++ flutterapp/lib/main.dart
@@ -2,6 +2,10 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
+import 'dart:math';
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -333,9 +337,75 @@ class BigCard extends StatelessWidget {
 }
 
 
-class MostrarImagen extends StatelessWidget {
+class MostrarImagen extends StatefulWidget {
+  @override
+  _MostrarImagenState createState() => _MostrarImagenState();
+}
+
+class _MostrarImagenState extends State<MostrarImagen> {
+  final List<String> _subdirectories = ['images', 'gifs', 'links'];
+  final Map<String, List<String>> _assets = {
+    'images': ['capi.jpg', 'hellLetLoose.jpg', 'tu-4.jpg'],
+    'gifs': ['barotrauma-baro.gif', 'barotrauma-barotrauma-game.gif', 'kitty-cat.gif'],
+  };
+
+  List<String> _links = [];
+  String? _selectedAsset;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadLinks();
+  }
+
+  Future<void> _loadLinks() async {
+    final String response = await rootBundle.loadString('assets/links/enlaces.json');
+    final data = await json.decode(response);
+    setState(() {
+      _links = List<String>.from(data['links']);
+      _selectRandomAsset();
+    });
+  }
+
+  void _selectRandomAsset() {
+    final random = Random();
+    final subdirectory = _subdirectories[random.nextInt(_subdirectories.length)];
+    if (subdirectory == 'links') {
+      final link = _links[random.nextInt(_links.length)];
+      setState(() {
+        _selectedAsset = link;
+      });
+    } else {
+      final assets = _assets[subdirectory]!;
+      final asset = assets[random.nextInt(assets.length)];
+      setState(() {
+        _selectedAsset = 'assets/$subdirectory/$asset';
+      });
+    }
+  }
+
+  Future<http.Response> _fetchImage(String url) {
+    return http.get(Uri.parse(url));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Image.asset('../assets/kitty-cat.gif');
+    return _selectedAsset == null
+        ? CircularProgressIndicator()
+        : _selectedAsset!.startsWith('http')
+            ? FutureBuilder<http.Response>(
+                future: _fetchImage(_selectedAsset!),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else if (snapshot.hasData) {
+                      return Image.memory(snapshot.data!.bodyBytes);
+                    }
+                  }
+                  return CircularProgressIndicator();
+                },
+              )
+            : Image.asset(_selectedAsset!);
   }
 }
